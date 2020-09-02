876a213cbb75762116d03d594ce24441cb71b4e8
AndrehW27 <andrehwillian@hotmail.com>
2020-09-02 18:22:36 -0300

    subindo atividade3 PI

---
+++ Tarefa 3 - Criação spring boot/impactmais/src/main/java/com/zeroseis/impactmais/ImpactmaisApplication.java
@@ -0,0 +1,13 @@
+package com.zeroseis.impactmais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpactmaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpactmaisApplication.class, args);
+	}
+
+}
---
+++ Tarefa 3 - Criação spring boot/impactmais/src/test/java/com/zeroseis/impactmais/ImpactmaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeroseis.impactmais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpactmaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
